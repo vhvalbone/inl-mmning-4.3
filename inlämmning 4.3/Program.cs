8258695e33dffc8079e018c4b91868863e271771
alsv06003 <alsv06003@E-PW0234JW>
2023-10-18 10:50:56 +0200

    Add project files.

---
+++ inlämmning 4.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace inlämmning_4._3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+
+    }
+}       
